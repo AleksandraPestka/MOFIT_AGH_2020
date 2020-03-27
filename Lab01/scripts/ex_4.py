8dfa6b6f87b6379f202a6b5f78df5fb1ece3c69b
AleksandraPestka <aleksandrapestka@wp.pl>
2020-03-27 09:39:47 +0100

    Exercise 4 works for delta time = 0.001

--- Lab01/scripts/ex_4.py
+++ Lab01/scripts/ex_4.py
@@ -4,32 +4,14 @@ import matplotlib.pyplot as plt
 from utils import V, V_deriv, V_double_deriv
 from config import E_0, v_0, m
 
-def Newton_system(F, J, x, x0, v, v0, eps):
+def trapezoidal_method(x_init, v_init, delta_time, alpha, max_iter, epsilon):
     """
-    Solve nonlinear system F=0 by Newton's method.
+    Solve nonlinear system F=0 by trapezoidal method.
     J is the Jacobian of F. Both F and J must be functions of x.
     At input, x holds the start value. The iteration continues
     until ||F|| < eps.
     """
-    F_value = F(x, x0, v, v0)
-    F_norm = np.linalg.norm(F_value, ord=2)  # l2 norm of vector
-    
-    iteration_counter = 0
-    while abs(F_norm) > eps and iteration_counter < 100:
-        delta = np.linalg.solve(J(x), -F_value)
-        x = x + delta
-        F_value = F(x)
-        F_norm = np.linalg.norm(F_value, ord=2)
-        iteration_counter += 1
 
-    # Here, either a solution is found, or too many iterations
-    if abs(F_norm) > eps:
-        iteration_counter = -1
-        
-    return x, iteration_counter
-
-def trapezoidal_method(x_init, v_init, delta_time, alpha, max_iter, epsilon):
-    
     def J(xn):
         """
         Calculate Jacobian matrix.
@@ -80,30 +62,33 @@ def trapezoidal_method(x_init, v_init, delta_time, alpha, max_iter, epsilon):
     v = v_init
     xn = x
     vn = v
-    
-    for _ in range(max_iter):
+
+    for num_attempt in range(max_iter):
+        print(f"\nAttempt number: {num_attempt}")
+        iter_counter = 0
         while abs(F1(xn, x, vn, v)) > epsilon or abs(F2(xn, x, vn, v)) > epsilon:
             delta = np.linalg.solve(J(xn), F(xn, x, vn, v))
+            print("Delta: ", delta) 
             xn += delta[0]
             vn += delta[1]
-            
+            iter_counter += 1
+
+        print(f"Number of iterations to converge: {iter_counter}")
         x = xn
         v = vn
     
     return x
     
-
-
 def main():
-    time_step = 0.01
+    time_step = 0.001
     alpha = 0                       #   damping factor
-    precision = 10e-10              #   acceptable precision
+    epsilon = 10e-8                 #   acceptable precision
     x_0 = 2.8325                    #   inital position
-    max_iteration = int(10e3)       #   maximum iteration number
+    max_iteration = 5               #   maximum iteration number
 
     x_solve = trapezoidal_method(x_0, v_0, 
-                                        time_step, alpha, max_iteration, precision)
-    print(x_solve)
+                                time_step, alpha, max_iteration, epsilon)
+    print("\nX computed solution: ", x_solve)
 
 if __name__ == "__main__":
     main()
---
+++ README.md
@@ -0,0 +1,3 @@
+# Numerical methods in physics and technology. 
+This repository contains work created during MOFIT course at AHG UST in 2019/20 academic year. 
+Our taks was to get to know the more popular numerical methods used in physics and technology as well as implementing some of them.
