d5801324382e4a90f4cb38442701d0cec67cfb27
AleksandraPestka <aleksandrapestka@wp.pl>
2020-04-29 22:23:35 +0200

    Lab04 done

--- Lab04/scripts/ex2.py
+++ Lab04/scripts/ex2.py
@@ -3,12 +3,70 @@ import random
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-
-from ex1 import metropolis_algorithm
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def wave_func(x, y):
     psi = np.pi**(-1/2) * np.exp(-(x**2 + y**2)/2)
     return abs(psi**2)
 
+def metropolis_algorithm_2D(x, y, dist_func):
+    delta_t_x = random.uniform(-1/4, 1/4)     
+    delta_t_y = random.uniform(-1/4, 1/4)
+    x_proposed = x + delta_t_x             
+    y_proposed = y + delta_t_y 
+        
+    z = random.uniform(0, 1)                
+
+    if z < (dist_func(x_proposed, y_proposed) / dist_func(x, y)):
+        return x_proposed, y_proposed
+   
+    return x,y
+
+def calc_energy(x_vec, y_vec, steps):
+    return (1/steps) * sum(np.add(np.square(x_vec), np.square(y_vec)))/2
+
 if __name__ == '__main__':
-    pass
+    MODE = 'energy'
+    # initial conditions
+    x, y = 0, 0
+    
+    if MODE == 'walk':
+        # first part
+        l = 100000
+        
+        x_list, y_list = [], []
+
+        for step in range(l):
+            x, y = metropolis_algorithm_2D(x, y, wave_func)
+            x_list.append(x)
+            y_list.append(y)
+
+        df = pd.DataFrame({'x': x_list, 'y': y_list})
+        df.to_csv('../data/ex2.csv')
+
+    elif MODE == 'energy':
+        # second part
+        df = pd.DataFrame(columns=['l', 'E'])
+
+        # l : number of steps
+        for l in tqdm(np.logspace(start=1, 
+                                stop=7, 
+                                endpoint=True,
+                                num=100,
+                                dtype='int32')):
+            df_row = []
+            df_row.append(l)
+            
+            x_list = []
+            y_list = []
+            for step in range(l):
+                x, y = metropolis_algorithm_2D(x, y, wave_func)
+                x_list.append(x)
+                y_list.append(y)
+
+            # calculate potential energy
+            df_row.append(calc_energy(x_list, y_list, l))
+            df.loc[len(df)] = df_row
+
+        df.to_csv('../data/ex2_energy.csv', index=False)
--- Lab04/scripts/plotting.py
+++ Lab04/scripts/plotting.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sns
+
+from ex2 import wave_func
 
 def plot_moments_vs_number_of_steps(dataframe):
     plt.figure()
@@ -15,6 +18,38 @@ def plot_moments_vs_number_of_steps(dataframe):
     plt.ylabel('moment value')
     plt.show()
 
+def plot_random_walk(no_steps, dist_fun, x_list, y_list):
+    x_vals = np.linspace(-3, 3, 1000)
+    y_vals = np.linspace(-3, 3, 1000)
+    X, Y = np.meshgrid(x_vals, y_vals)
+    Z = [dist_fun(i,j) for (i,j) in zip(X, Y)]
+
+    plt.figure()
+    cp = plt.contourf(X, Y, Z)
+    plt.colorbar(cp, label='distribution function values')
+    plt.plot(x_list[:no_steps], y_list[:no_steps], c='k', 
+             alpha=0.5, label='random walk')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.show()
+
+def plot_energy_vs_number_of_steps(dataframe):
+    plt.figure()
+    plt.xscale('log')
+    plt.plot(dataframe['l'], dataframe['E'])   
+    plt.legend()
+    plt.grid()
+    plt.xlabel('no. of steps')
+    plt.ylabel('E')
+    plt.show()
+
 if __name__ == '__main__':
     df = pd.read_csv('../data/ex1.csv', header=0, index_col=None)
     plot_moments_vs_number_of_steps(df)
+
+    df2 = pd.read_csv('../data/ex2.csv', header=0, index_col=None)
+    plot_random_walk(10000, wave_func, df2['x'], df2['y'])
+
+    df3 = pd.read_csv('../data/ex2_energy.csv', header=0, index_col=None)
+    plot_energy_vs_number_of_steps(df3)
